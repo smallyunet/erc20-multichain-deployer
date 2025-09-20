d36d4f7522ee913d9340394c01118f3cbfd4a211
smallyunet <dev.wangyu@gmail.com>
2025-09-20 23:06:21 +0800

    verify via etherscan v2

--- verify.sh
+++ verify.sh
@@ -5,13 +5,13 @@ ARGS=$(cast abi-encode \
   "$TOKEN_NAME" "$TOKEN_SYMBOL" "$TOKEN_DECIMALS" "$OWNER" "$SUPPLY_RAW")
 
 forge verify-contract \
-  --chain base-sepolia \
-  --verifier-url "$BASESCAN_SEPOLIA_API" \
-  --etherscan-api-key "$BASESCAN_API_KEY" \
+  --watch \
+  --chain 80002 \
+  0x123c6CD93AC01A892277BB36424467ca6e53ca23 \
+  src/MyToken.sol:MyToken \
+  --verifier etherscan \
+  --etherscan-api-key "$ETHERSCAN_API_KEY" \
+  --constructor-args $ARGS \
   --compiler-version v0.8.24 \
   --num-of-optimizations 200 \
-  --evm-version cancun \
-  0xYourDeployedTokenAddressHere \
-  src/MyToken.sol:MyToken \
-  --constructor-args $ARGS \
-  --watch
+  --evm-version cancun
